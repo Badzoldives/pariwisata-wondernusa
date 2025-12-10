1a177fa1b205d52185554cb4384c4576414078ab
name <email>
2025-12-10 17:39:18 +0800

    push lagi

--- wondernusa/src/router/index.js
+++ wondernusa/src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -23,11 +23,6 @@ const router = createRouter({
       path: '/accommodations',
       name: 'accommodations',
       component: () => import('../views/AccommodationsView.vue'),
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
     },
     {
       path: '/contact',
--- wondernusa/vite.config.js
+++ wondernusa/vite.config.js
@@ -7,6 +7,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+
+base: '/pariwisata-wondernusa/',
+
   plugins: [
     vue(),
     vueDevTools(),
